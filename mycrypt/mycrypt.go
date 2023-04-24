97d55e6b7988575b8ecf482db9fcd72b6c50166c
Omar Passi <omar@Omars-MBP.uia.no>
2023-04-24 08:30:30 +0200

    la til mod

--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -2,14 +2,14 @@ package mycrypt
 
 var ALF_SEM03 []rune = []rune("abcdefghijklmnopqrstuvwxyzæøå0123456789.,:; ")
 
-func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
+func Krypter(melding []rune, chiffer int) []rune {
 	kryptertMelding := make([]rune, len(melding))
 	for i := 0; i < len(melding); i++ {
-		indeks := sokIAlfabetet(melding[i], alphabet)
-		if indeks+chiffer >= len(alphabet) {
-			kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
+		indeks := sokIAlfabetet(melding[i], ALF_SEM03)
+		if indeks+chiffer >= len(ALF_SEM03) {
+			kryptertMelding[i] = ALF_SEM03[indeks+chiffer-len(ALF_SEM03)]
 		} else {
-			kryptertMelding[i] = alphabet[indeks+chiffer]
+			kryptertMelding[i] = ALF_SEM03[indeks+chiffer]
 		}
 	}
 	return kryptertMelding
@@ -19,7 +19,6 @@ func sokIAlfabetet(symbol rune, alfabet []rune) int {
 	for i := 0; i < len(alfabet); i++ {
 		if symbol == alfabet[i] {
 			return i
-			break
 		}
 	}
 	return -1
